f8682153b9e3e9bc81e4d0539c1449f5d9aa66e5
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2017-01-14 18:52:43 -0600

    moved content coordinate

--- imageProcessor.js
+++ imageProcessor.js
@@ -83,23 +83,6 @@
             var coordinate = new Coordinate(x,y);
             return coordinate;
         }
-        /**
-        A content(element) at a given coordinate.
-        **/
-        function ContentCoordinate(x,y,content,fontSize) {
-            this.content = content;
-            this.coordinate = new Coordinate(x,y);
-            this.fontSize=fontSize;
-            this.draw=function(canvas,align) {
-                addletter(canvas, this.content,
-                 this.coordinate.x, this.coordinate.y,this.fontSize,align);
-            }
-            this.printValue = function() {
-                return "ContentCoordinate["+this.coordinate.printValue()
-                +",Content:"+this.content+" at Size:"+this.fontSize+"]";
-            }
-        }
-
         function ContentRectangle(minX,minY,maxX,maxY) {
             //top left coordinate.
             this.min = new Coordinate(minX,minY);
