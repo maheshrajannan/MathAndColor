bf87175a38b4451d2bb2bc7991f6a57b517e96bd
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2017-01-14 18:41:25 -0600

    moved coordinate to separate function

--- imageProcessor.js
+++ imageProcessor.js
@@ -83,28 +83,6 @@
             var coordinate = new Coordinate(x,y);
             return coordinate;
         }
-
-        function Coordinate(x,y) {
-            //Will become an attribute of every instance of coordinate.
-            this.x=Math.round(x);
-            this.y=Math.round(y);
-            this.printValue=function(){
-                return "Coordinate=[x="+this.x+",y="+this.y+"]";
-            }
-            this.getNumberX= function() {
-                return Number(x);
-            }
-            this.getNumberY= function() {
-                return Number(y);
-            }
-            this.setX=function(x1) {
-                this.x=Math.round(x1);
-            }
-            this.setY=function(y1) {
-                this.y=Math.round(y1);
-            }
-        }
-
         /**
         A content(element) at a given coordinate.
         **/
